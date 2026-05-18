ac5d978c440b3a64fa3e7c511e547f6561829928
Sathyarjun <Sathya@62710>
2026-05-19 00:21:56 +0530

    feat:logout function in context provider, get user server action.

--- app/AuthContextProvider.tsx
+++ app/AuthContextProvider.tsx
@@ -1,21 +1,61 @@
 "use client"
 
-import { createContext, useContext, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { UserModel } from "./generated/prisma/models";
+import getProfile from "./serverAction/getUser";
+import logoutAction from "./serverAction/logout";
 
-const AuthContext = createContext({
-    user: {} as UserModel | null,
-    token: null as string | null,
-})
+type AuthContextType = {
+    user: UserModel | null
+    refreshProfile: () => Promise<void>
+    logout: () => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextType | null>(null)
 
 export const AuthProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 
-    const [user, setUser] = useState<UserModel | null>(null)
-    const [token, setToken] = useState<string | null>(null)
+    const router = useRouter()
 
-    const authValue = useMemo(() => ({ user, token }), [user, token])
+    const [user, setUser] = useState<UserModel | null>(null)
+
+    const refreshProfile = async () => {
+        try {
+            const profile = await getProfile()
+            if (!profile.success) throw new Error(profile.data)
+            setUser(profile.data)
+        } catch (err) {
+            await logout()
+        }
+    }
+
+    useEffect(() => {
+        refreshProfile()
+    }, []);
+
+
+    const logout = async () => {
+        await logoutAction()
+        setUser(null)
+        router.push("/login")
+    }
+
+
+
+    const authValue = useMemo(() => ({ user, refreshProfile, logout }), [user])
 
     return <AuthContext.Provider value={authValue} >{children}</AuthContext.Provider>
 }
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+
+    if (!context) {
+        throw new Error(
+            "useAuth must be used within AuthProvider"
+        )
+    }
+
+    return context
+}
